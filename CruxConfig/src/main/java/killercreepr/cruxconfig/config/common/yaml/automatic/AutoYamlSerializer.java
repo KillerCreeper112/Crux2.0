ac25035b59666d04ea740ac15b186964ebfc715a
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-05-23 13:38:06 -0400

    Ye

--- CruxConfig/src/main/java/killercreepr/cruxconfig/config/common/yaml/automatic/AutoYamlSerializer.java
+++ CruxConfig/src/main/java/killercreepr/cruxconfig/config/common/yaml/automatic/AutoYamlSerializer.java
@@ -10,28 +10,60 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class AutoYamlSerializer<T> implements YamlObjectHandler<T> {
     public static <T> AutoYamlSerializer<T> notNull(@NotNull Class<T> type){
         return new AutoYamlSerializer<>(type, (name, value) -> value != null);
     }
 
+    public static <T> AutoYamlSerializer<T> withDisabledFields(@NotNull Class<T> type, @NotNull String @NotNull... disabledFields){
+        Stream<String> stream = Arrays.stream(disabledFields);
+        return new AutoYamlSerializer<>(type, null, (field) -> stream.anyMatch(x -> x.equals(field.getName())));
+    }
+
     protected final @NotNull Class<T> type;
     protected final @Nullable BiPredicate<String, Object> isValid;
-    public AutoYamlSerializer(@NotNull Class<T> type, @Nullable BiPredicate<String, Object> isValid) {
+    protected final @Nullable Predicate<Field> disabledFields;
+    public AutoYamlSerializer(@NotNull Class<T> type, @Nullable BiPredicate<String, Object> isValid, @Nullable Predicate<Field> disabledFields) {
         this.type = type;
         this.isValid = isValid;
+        this.disabledFields = disabledFields;
+    }
+
+    public AutoYamlSerializer(@NotNull Class<T> type, @Nullable BiPredicate<String, Object> isValid) {
+        this(type, isValid, null);
     }
 
     public AutoYamlSerializer(@NotNull Class<T> type) {
-        this(type, null);
+        this(type, null, null);
     }
 
     public @NotNull Class<T> getType() {
         return type;
+    }
+
+    /**
+     * @return A predicate that determines whether a Field's object
+     * is valid when deserializing. If any of the fields are not valid,
+     * the deserializeFromYaml method will return null.
+     */
+    public @Nullable BiPredicate<String, Object> getIsValid() {
+        return isValid;
+    }
+
+    /**
+     * @return A predicate that determines whether a Field should be
+     * deserialized and serialized. If the predicate returns
+     * true on a Field, that Field will not be saved or loaded.
+     */
+    public @Nullable Predicate<Field> getDisabledFields() {
+        return disabledFields;
     }
 
     @Override
@@ -39,6 +71,7 @@ public class AutoYamlSerializer<T> implements YamlObjectHandler<T> {
         YamlObject map = new YamlObject();
         YamlRegistry registry = context.getRegistry();
         for(Field field : CruxReflect.getNonStaticDeclaredFields(object.getClass())){
+            if(disabledFields != null && disabledFields.test(field)) continue;
             try{
                 boolean x = field.canAccess(object);
                 field.setAccessible(true);
@@ -59,6 +92,7 @@ public class AutoYamlSerializer<T> implements YamlObjectHandler<T> {
         Map<String, Object> fields = new LinkedHashMap<>();
         Map<String, YamlElement> yamlMap = o.asMap();
         for(Field field : CruxReflect.getNonStaticDeclaredFields(type)){
+            if(disabledFields != null && disabledFields.test(field)) continue;
             Object found  = registry.deserialize(field.getType(), yamlMap.get(field.getName()));
             if(isValid != null && !isValid.test(field.getName(), found)) return null;
             fields.put(field.getName(), found);
