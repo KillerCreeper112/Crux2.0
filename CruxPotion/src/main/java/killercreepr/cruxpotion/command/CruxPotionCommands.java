906b293e5b5e5ca44c3570bcdc553a5c04c27503
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-05-31 23:07:47 -0400

    ye boi

--- CruxPotion/src/main/java/killercreepr/cruxpotion/command/CruxPotionCommands.java
+++ CruxPotion/src/main/java/killercreepr/cruxpotion/command/CruxPotionCommands.java
@@ -7,27 +7,20 @@ import io.papermc.paper.command.brigadier.Commands;
 import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
 import io.papermc.paper.command.brigadier.argument.range.IntegerRangeProvider;
 import io.papermc.paper.command.brigadier.argument.resolvers.selector.EntitySelectorArgumentResolver;
-import io.papermc.paper.plugin.lifecycle.event.LifecycleEvent;
 import io.papermc.paper.plugin.lifecycle.event.LifecycleEventManager;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import killercreepr.crux.data.entity.EntityMemory;
 import killercreepr.crux.plugin.CruxPlugin;
-import killercreepr.cruxpotion.CruxPotionModule;
 import killercreepr.cruxpotion.command.args.CruxPotionArgument;
-import killercreepr.cruxpotion.command.suggestions.CruxPotionSuggester;
 import killercreepr.cruxpotion.data.PotionHolder;
-import killercreepr.cruxpotion.event.EntityCruxPotionEvent;
 import killercreepr.cruxpotion.potions.ActivePotion;
 import killercreepr.cruxpotion.potions.CruxPotion;
 import killercreepr.cruxpotion.potions.inflictor.BlockInflictor;
 import killercreepr.cruxpotion.potions.inflictor.EntityInflictor;
 import killercreepr.cruxpotion.potions.inflictor.PotionInflictor;
-import net.minecraft.server.commands.EffectCommands;
-import org.apache.logging.log4j.core.LifeCycle;
 import org.bukkit.block.Block;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
-import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -38,7 +31,7 @@ public class CruxPotionCommands {
     public static void register(@NotNull CruxPlugin plugin){
         plugin.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, event ->{
             final Commands commands = event.registrar();
-            LiteralCommandNode<CommandSourceStack> cmd = build(Commands.literal("cruxpotion"), plugin.getLifecycleManager());
+            LiteralCommandNode<CommandSourceStack> cmd = build(Commands.literal("ayotest"), plugin.getLifecycleManager());
             commands.register(cmd);
         });
     }
@@ -53,7 +46,6 @@ public class CruxPotionCommands {
                                 Commands.argument("entity", ArgumentTypes.entities())
                                         .then(
                                                 Commands.argument("potion", CruxPotionArgument.cruxPotion())
-                                                        .suggests(new CruxPotionSuggester())
                                                         .executes(ctx ->{
                                                             CruxPotion potion = ctx.getArgument("potion",
                                                                     CruxPotion.class);
--- CruxPotion/src/main/java/killercreepr/cruxpotion/command/args/CruxPotionArgument.java
+++ CruxPotion/src/main/java/killercreepr/cruxpotion/command/args/CruxPotionArgument.java
@@ -2,12 +2,23 @@ package killercreepr.cruxpotion.command.args;
 
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.suggestion.Suggestion;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
 import io.papermc.paper.command.brigadier.argument.CustomArgumentType;
 import killercreepr.cruxpotion.potions.CruxPotion;
 import killercreepr.cruxpotion.registries.CruxPotionRegistries;
 import net.kyori.adventure.key.Key;
+import net.minecraft.commands.synchronization.SuggestionProviders;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
 
 public class CruxPotionArgument implements CustomArgumentType<CruxPotion, Key> {
     private static final CruxPotionArgument CRUX_POTION = new CruxPotionArgument();
@@ -16,13 +27,21 @@ public class CruxPotionArgument implements CustomArgumentType<CruxPotion, Key> {
     }
 
     @Override
-    public @NotNull CruxPotion parse(@NotNull StringReader reader) {
-        Key key = Key.key(reader.getString());
+    public @NotNull CruxPotion parse(@NotNull StringReader reader) throws CommandSyntaxException {
+        Key key = Key.key(reader.readString());
         return CruxPotionRegistries.POTIONS.get(key);
     }
 
     @Override
     public @NotNull ArgumentType<Key> getNativeType() {
         return ArgumentTypes.key();
+    }
+
+    @Override
+    public @NotNull <S> CompletableFuture<Suggestions> listSuggestions(@NotNull CommandContext<S> context, @NotNull SuggestionsBuilder builder) {
+        for(CruxPotion p : CruxPotionRegistries.POTIONS){
+            builder.suggest(p.key().asMinimalString());
+        }
+        return builder.buildFuture();
     }
 }
