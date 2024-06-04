eeca72258d6c0a4371a587083cde802c15c1f775
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-06-04 13:45:42 -0400

    changes

---
+++ CruxEntities/src/main/java/killercreepr/cruxentities/CruxEntitiesModule.java
@@ -0,0 +1,21 @@
+package killercreepr.cruxentities;
+
+import killercreepr.crux.module.CruxModule;
+import killercreepr.crux.plugin.CruxPlugin;
+import killercreepr.cruxentities.listener.EntityManager;
+import org.jetbrains.annotations.NotNull;
+
+public class CruxEntitiesModule implements CruxModule {
+    public static final String NAMESPACE = "CruxEntities";
+    @Override
+    public @NotNull String name() {
+        return NAMESPACE;
+    }
+
+    @Override
+    public void onEnable(@NotNull CruxPlugin plugin) {
+        plugin.registerListeners(
+            new EntityManager(plugin)
+        );
+    }
+}
---
+++ CruxEntities/src/main/java/killercreepr/cruxentities/listener/EntityManager.java
@@ -0,0 +1,35 @@
+package killercreepr.cruxentities.listener;
+
+import killercreepr.cruxentities.entity.CruxMob;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.world.EntitiesLoadEvent;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+public class EntityManager implements Listener {
+    protected final Plugin plugin;
+
+    public EntityManager(Plugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onEntitiesLoad(EntitiesLoadEvent event) {
+        plugin.getServer().getScheduler().runTask(plugin, task ->{
+            for(Entity e : event.getEntities()){
+                if(!e.isValid()) continue;
+                loadEntity(e);
+            }
+        });
+    }
+
+    public static void loadEntity(@NotNull Entity e){
+        if(e.getType() == EntityType.UNKNOWN) return;
+        CruxMob mob = CruxMob.get(e);
+        if(mob==null) return;
+        mob.load(e);
+    }
+}
