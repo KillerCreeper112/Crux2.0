2508984b3c15c221df221ab83dcb82f8b2a774ab
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-06-06 11:39:17 -0400

    changes

---
+++ CruxEntities/build.gradle.kts
@@ -0,0 +1,16 @@
+
+version = "1.0"
+plugins {
+    alias(libs.plugins.paperweight)
+    alias(libs.plugins.shadow)
+}
+
+dependencies {
+    paperweight.paperDevBundle(libs.versions.paper)
+    compileOnly(project(":CruxMain"))
+    compileOnly(project(":CruxAttributes"))
+
+    compileOnly(fileTree("libs") {
+        include("*.jar")
+    })
+}
