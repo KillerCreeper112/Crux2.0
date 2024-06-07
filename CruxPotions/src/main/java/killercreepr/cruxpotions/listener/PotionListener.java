5af449b4090ad13f3b042d96a1e5ddc5138c2314
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-06-07 19:17:20 -0400

    changes

--- CruxPotions/src/main/java/killercreepr/cruxpotions/listener/PotionListener.java
+++ CruxPotions/src/main/java/killercreepr/cruxpotions/listener/PotionListener.java
@@ -2,21 +2,28 @@ package killercreepr.cruxpotions.listener;
 
 import com.destroystokyo.paper.event.entity.WitchConsumePotionEvent;
 import killercreepr.crux.data.entity.EntityMemory;
-import killercreepr.crux.data.entity.PlayerMemory;
 import killercreepr.cruxpotions.CruxPotions;
 import killercreepr.cruxpotions.data.PotionHolder;
 import killercreepr.cruxpotions.persistence.PotionPersistTags;
 import killercreepr.cruxpotions.persistence.StoredPotion;
+import killercreepr.cruxpotions.potions.inflictor.EntityInflictor;
+import killercreepr.cruxpotions.potions.inflictor.PotionInflictor;
 import killercreepr.cruxpotions.values.ValuesProvider;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.ThrownPotion;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.AreaEffectCloudApplyEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.LingeringPotionSplashEvent;
+import org.bukkit.event.entity.PotionSplashEvent;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 
@@ -36,29 +43,68 @@ public class PotionListener implements Listener {
         }
     }
 
-    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    public void applyPotion(@NotNull Entity p, @Nullable ItemStack item){
+        if(item==null) return;
+        if(!CruxPotions.canApplyPotion(p)) return;
+        PotionHolder data = EntityMemory.getOrCreateDataHolder(p, PotionHolder.class);
+        if(data==null) return;
+        Collection<StoredPotion> potions = PotionPersistTags.STORED_CUSTOM_POTIONS.get(item, null);
+        if(potions == null || potions.isEmpty()) return;
+        PotionInflictor inflictor = new EntityInflictor(p);
+        for(StoredPotion h : potions){
+            data.addPotion(h.create(p, inflictor));
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
     public void onPlayerItemConsume(PlayerItemConsumeEvent event) {
         Player p = event.getPlayer();
-        if(!CruxPotions.canApplyPotion(p)) return;
-        PlayerMemory memory = PlayerMemory.getOrCreate(p);
-        if(!(memory.getHolder(PotionHolder.KEY) instanceof PotionHolder data)) return;
-        Collection<StoredPotion> potions = PotionPersistTags.STORED_CUSTOM_POTIONS.get(event.getItem(), null);
-        if(potions == null || potions.isEmpty()) return;
-        for(StoredPotion h : potions){
-            data.addPotion(h.create(p));
-        }
+        applyPotion(p, event.getItem());
     }
 
     @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
     public void onWitchConsumePotion(WitchConsumePotionEvent event) {
         Entity p = event.getEntity();
-        if(!CruxPotions.canApplyPotion(p)) return;
-        EntityMemory memory = EntityMemory.getOrCreate(p);
-        if(!(memory.getHolder(PotionHolder.KEY) instanceof PotionHolder data)) return;
-        Collection<StoredPotion> potions = PotionPersistTags.STORED_CUSTOM_POTIONS.get(event.getPotion(),null);
+        applyPotion(p, event.getPotion());
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
+    public void onPotionSplash(PotionSplashEvent event) {
+        if(event.getAffectedEntities().isEmpty()) return;
+        ThrownPotion potion = event.getPotion();
+        ItemStack item = potion.getItem();
+        Collection<StoredPotion> potions = PotionPersistTags.STORED_CUSTOM_POTIONS.get(item, null);
         if(potions == null || potions.isEmpty()) return;
-        for(StoredPotion h : potions){
-            data.addPotion(h.create(p));
+        PotionInflictor inflictor = new EntityInflictor(potion);
+        for(Entity e : event.getAffectedEntities()){
+            PotionHolder data = EntityMemory.getOrCreateDataHolder(e, PotionHolder.class);
+            if(data==null) continue;
+            for(StoredPotion h : potions){
+                data.addPotion(h.create(e, inflictor));
+            }
+        }
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
+    public void onLingeringPotionSplash(LingeringPotionSplashEvent event) {
+        Collection<StoredPotion> potions = PotionPersistTags.STORED_CUSTOM_POTIONS.get(event.getEntity().getItem(), null);
+        if(potions == null || potions.isEmpty()) return;
+        Entity cloud = event.getAreaEffectCloud();
+        PotionPersistTags.STORED_CUSTOM_POTIONS.set(cloud, potions);
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
+    public void onAreaEffectCloudApply(AreaEffectCloudApplyEvent event) {
+        Entity cloud = event.getEntity();
+        Collection<StoredPotion> potions = PotionPersistTags.STORED_CUSTOM_POTIONS.get(cloud, null);
+        if(potions == null || potions.isEmpty()) return;
+        PotionInflictor inflictor = new EntityInflictor(cloud);
+        for(Entity e : event.getAffectedEntities()){
+            PotionHolder data = EntityMemory.getOrCreateDataHolder(e, PotionHolder.class);
+            if(data==null) continue;
+            for(StoredPotion h : potions){
+                data.addPotion(h.create(e, inflictor));
+            }
         }
     }
 }
