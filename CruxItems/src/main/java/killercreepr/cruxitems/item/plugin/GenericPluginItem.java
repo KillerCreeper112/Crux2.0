52c0c15af9b02d9f5e7afcb707dcfd5e08c9676e
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-06-07 21:34:15 -0400

    changes

--- CruxItems/src/main/java/killercreepr/cruxitems/item/plugin/GenericPluginItem.java
+++ CruxItems/src/main/java/killercreepr/cruxitems/item/plugin/GenericPluginItem.java
@@ -1,13 +1,30 @@
 package killercreepr.cruxitems.item.plugin;
 
+import killercreepr.crux.Crux;
+import killercreepr.crux.tags.container.StringHookContainer;
+import killercreepr.crux.util.CruxString;
 import net.kyori.adventure.key.Key;
+import org.bukkit.entity.Entity;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public abstract class GenericPluginItem implements PluginItem{
     protected final @NotNull Key key;
     public GenericPluginItem(@NotNull Key key) {
         this.key = key;
     }
+
+    @Override
+    public @NotNull ItemStack buildItem(@Nullable Entity holder, @Nullable StringHookContainer tags) {
+        ItemStack item = buildGeneric(holder, tags);
+        item.editMeta(meta ->{
+            meta.displayName(Crux.FORMAT.deserialize(CruxString.toTitleCase(key.value())));
+        });
+        return item;
+    }
+
+    public abstract @NotNull ItemStack buildGeneric(@Nullable Entity holder, @Nullable StringHookContainer stringHookContainer);
 
     @Override
     public @NotNull Key key() {
