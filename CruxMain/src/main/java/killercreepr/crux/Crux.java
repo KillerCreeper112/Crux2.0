7c2846be286216728f8acf1bf31900d5485b1547
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-06-20 22:42:11 -0400

    removed hooked tags

--- CruxMain/src/main/java/killercreepr/crux/Crux.java
+++ CruxMain/src/main/java/killercreepr/crux/Crux.java
@@ -54,44 +54,6 @@ public final class Crux {
 
     private static final @NotNull CruxHandlers handlers = new CruxHandlers.Generic();
     public static @NotNull CruxHandlers handlers(){ return handlers; }
-    /*private final MenuRegistry MENU_REGISTRY = new MenuRegistry(FORMAT);
-
-    public @NotNull MenuRegistry getMenuRegistry() {
-        return MENU_REGISTRY;
-    }*/
-
-    /*public void onEnable() {
-        if(getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")){
-            placeholderAPIHook = new PlaceholderAPIHook();
-        }else placeholderAPIHook = null;
-
-        *//*Registry<MenuAction> actions = MENU_REGISTRY.MENU_ACTIONS;
-        actions.register(new OpenMenuAction(key("menu")));
-        actions.register(new UpdateMenuAction(key("update")));
-        actions.register(new SoundAction(key("sound")));
-        actions.register(new CloseInventoryAction(key("close")));
-
-        new MenuFolder(this, "menu", getMenuRegistry()).register();*//*
-
-        new CClaimTags(this, FORMAT.getTags());
-
-        //getServer().getPluginManager().registerEvents(new MenuListener(), this);
-    }*/
-
-    /*@EventHandler(ignoreCancelled = true)
-    public void onPlayerSwapHandItems(PlayerSwapHandItemsEvent event) {
-        new MenuFolder(this, "menu", getMenuRegistry()).register();
-    }
-
-    @EventHandler(ignoreCancelled = true)
-    public void onAsyncChat(AsyncChatEvent event) {
-        Player p = event.getPlayer();
-        String plain = PlainTextComponentSerializer.plainText().serialize(event.message());
-        Bukkit.getScheduler().runTask(this, task ->{
-            MenuHolder menu = MENU_REGISTRY.MENU_HOLDERS.get(Crux.key(plain));
-            if(menu != null) menu.open(p);
-        });
-    }*/
 
     public static @NotNull BukkitRunnable buildTickRunnable(){
         return buildTickRunnable(CruxRegistries.TICKS, true);
