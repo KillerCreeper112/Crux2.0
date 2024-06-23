abc6449f550d486234fd5be9978b8cab145c0f57
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-06-23 15:44:39 -0400

    changes

--- CruxMenus/src/main/java/killercreepr/cruxmenus/menu/bukkit/slot/TempStoredSlot.java
+++ CruxMenus/src/main/java/killercreepr/cruxmenus/menu/bukkit/slot/TempStoredSlot.java
@@ -1,7 +1,6 @@
 package killercreepr.cruxmenus.menu.bukkit.slot;
 
 import killercreepr.crux.util.CruxEntity;
-import killercreepr.crux.util.CruxItem;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
@@ -10,7 +9,7 @@ public interface TempStoredSlot extends Slot{
     @Override
     default void onMenuClose(@NotNull Player p) {
         ItemStack item = getItem();
-        if(CruxItem.isEmpty(item) || isSlottedItem(item)) return;
+        if(isBlank(item) || item == null) return;
         CruxEntity.giveOrDrop(p, item);
     }
 }
