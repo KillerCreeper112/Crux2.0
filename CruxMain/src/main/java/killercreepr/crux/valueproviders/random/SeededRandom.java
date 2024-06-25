1058abef8346774fb4864594e1c35b4e38cdbd97
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-06-25 08:55:59 -0400

    changes

--- CruxMain/src/main/java/killercreepr/crux/valueproviders/random/SeededRandom.java
+++ CruxMain/src/main/java/killercreepr/crux/valueproviders/random/SeededRandom.java
@@ -1,35 +1,18 @@
 package killercreepr.crux.valueproviders.random;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.util.Random;
 
-public class SeededRandom implements RandomHolder{
-    protected final @NotNull Random random;
+public class SeededRandom extends Random{
     protected long seed;
-
-    public SeededRandom(@NotNull Random random, long seed) {
-        this.random = random;
-        this.seed = seed;
-        random.setSeed(seed);
-    }
-
     public SeededRandom(long seed) {
-        this(new Random(), seed);
+        super(seed);
+        this.seed = seed;
     }
 
-    public SeededRandom setSeed(long seed){
-        this.seed = seed;
-        random.setSeed(seed);
-        return this;
+    public SeededRandom(){
     }
 
     public long getSeed() {
         return seed;
-    }
-
-    @Override
-    public @NotNull Random value() {
-        return random;
     }
 }
