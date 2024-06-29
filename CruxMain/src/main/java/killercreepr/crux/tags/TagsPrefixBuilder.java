95ee64f7f632b484d74a72f0d4e39cf9e755034c
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-06-29 11:50:03 -0400

    previous commit was object hook overhaul

--- CruxMain/src/main/java/killercreepr/crux/tags/TagsPrefixBuilder.java
+++ CruxMain/src/main/java/killercreepr/crux/tags/TagsPrefixBuilder.java
@@ -22,27 +22,23 @@ public interface TagsPrefixBuilder {
             }
 
             @Override
-            public @Nullable <T> FormatPrefix buildHookedPrefix(@NotNull ObjectTag<T> tag, @NotNull T object, @Nullable TagContainer<?> tags) {
-                return TagsPrefixBuilder.super.buildHookedPrefix(tag, object, tags);
+            public <T> @NotNull FormatPrefix buildHookedPrefix(@NotNull ObjectTag<T> baseTag, @NotNull T baseObject, @NotNull HookedObjectTag<?, ?> tag) {
+                return new FormatPrefix() {
+                    @Override
+                    public @NotNull String buildPrefix(@NotNull TagResolver<?> resolver) {
+                        return prefix +
+                            baseTag.defaultPrefix().buildPrefix(resolver) +
+                            tag.getObjectTag().defaultPrefix().buildPrefix(resolver);
+                    }
+                };
             }
-
-            /*@Override
-            public @NotNull String prefix(@NotNull ObjectTag<?> tag, @NotNull Object object) {
-                return prefix + tag.defaultPrefix().prefix(tag, object);
-            }
-
-            @Override
-            public @NotNull String hookedPrefix(@NotNull ObjectTag<?> mainTag, @NotNull Object object, @NotNull ObjectHookContainer<?> hooked) {
-                return prefix + mainTag.defaultPrefix().prefix(mainTag, object) +
-                    hooked.getObjectTag().defaultPrefix().prefix(hooked.getObjectTag(), hooked.getObject());
-            }*/
         };
     }
 
     static @NotNull TagsPrefixBuilder addon(@NotNull String prefix){
         return new TagsPrefixBuilder() {
             @Override
-            public @Nullable <T> FormatPrefix buildPrefix(@NotNull ObjectTag<T> tag, @NotNull T object, @Nullable TagContainer<?> tags) {
+            public <T> @NotNull FormatPrefix buildPrefix(@NotNull ObjectTag<T> tag, @NotNull T object, @Nullable TagContainer<?> tags) {
                 return new FormatPrefix() {
                     @Override
                     public @NotNull String buildPrefix(@NotNull TagResolver<?> resolver) {
@@ -51,26 +47,18 @@ public interface TagsPrefixBuilder {
                 };
             }
 
-        };
-    }
-
-    static @NotNull TagsPrefixBuilder addon(@NotNull String prefix) {
-        return new FormatPrefix() {
             @Override
-            public @NotNull String prefix(@NotNull ObjectTag<?> tag, @NotNull Object object) {
-                return prefix + tag.defaultPrefix().prefix(tag, object);
-            }
-
-            @Override
-            public @NotNull String hookedPrefix(@NotNull ObjectTag<?> mainTag, @NotNull Object object, @NotNull ObjectHookContainer<?> hooked) {
-                return prefix +
-                    hooked.getObjectTag().defaultPrefix().prefix(hooked.getObjectTag(), hooked.getObject());
+            public <T> @NotNull FormatPrefix buildHookedPrefix(@NotNull ObjectTag<T> baseTag, @NotNull T baseObject, @NotNull HookedObjectTag<?, ?> tag) {
+                return new FormatPrefix() {
+                    @Override
+                    public @NotNull String buildPrefix(@NotNull TagResolver<?> resolver) {
+                        return prefix + tag.getObjectTag().defaultPrefix().buildPrefix(resolver);
+                    }
+                };
             }
         };
     }
 
     <T> @Nullable FormatPrefix buildPrefix(@NotNull ObjectTag<T> tag, @NotNull T object, @Nullable TagContainer<?> tags);
-    default <T> @Nullable FormatPrefix buildHookedPrefix(@NotNull ObjectTag<T> baseTag, @NotNull T baseObject, @NotNull HookedObjectTag<?, ?> tag){
-        return buildPrefix(baseTag, baseObject, tag.getTags());
-    }
+    <T> @Nullable FormatPrefix buildHookedPrefix(@NotNull ObjectTag<T> baseTag, @NotNull T baseObject, @NotNull HookedObjectTag<?, ?> tag);
 }
