93fff5e2ac99e3ef22300703db0cec4b82491c2d
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-07-02 16:04:59 -0400

    ye

---
+++ CruxMenus/src/main/java/killercreepr/cruxmenus/menu/bukkit/module/SimpleMenuModuleBuilder.java
@@ -0,0 +1,19 @@
+package killercreepr.cruxmenus.menu.bukkit.module;
+
+import killercreepr.cruxconfig.config.common.FileContext;
+import killercreepr.cruxconfig.config.common.element.FileElement;
+import killercreepr.cruxconfig.config.common.element.FileObject;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public interface SimpleMenuModuleBuilder extends MenuModuleBuilder{
+    @Override
+    default @Nullable MenuModule build(@NotNull FileContext<?> context, @NotNull FileElement e, @Nullable FileObject menuContext) {
+        if(!(e instanceof FileObject o)) return null;
+        String id = o.getObject(String.class, "id");
+        if(id==null) return null;
+        return build(id, context, o, menuContext);
+    }
+
+    @Nullable MenuModule build(@NotNull String id, @NotNull FileContext<?> context, @NotNull FileObject o, @Nullable FileObject menuContext);
+}
--- CruxMenus/src/main/java/killercreepr/cruxmenus/menu/bukkit/module/standard/PagedMenuModuleBuilder.java
+++ CruxMenus/src/main/java/killercreepr/cruxmenus/menu/bukkit/module/standard/PagedMenuModuleBuilder.java
@@ -1,29 +1,18 @@
 package killercreepr.cruxmenus.menu.bukkit.module.standard;
 
-import killercreepr.crux.Crux;
 import killercreepr.crux.valueproviders.number.NumberProvider;
 import killercreepr.cruxconfig.config.common.FileContext;
 import killercreepr.cruxconfig.config.common.element.FileElement;
 import killercreepr.cruxconfig.config.common.element.FileObject;
-import killercreepr.cruxmenus.menu.bukkit.module.MenuModule;
-import killercreepr.cruxmenus.menu.bukkit.module.MenuModuleBuilder;
-import net.kyori.adventure.key.Key;
+import killercreepr.cruxmenus.menu.bukkit.module.SimpleMenuModuleBuilder;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class PagedMenuModuleBuilder implements MenuModuleBuilder {
-    @Override
-    public @Nullable MenuModule build(@NotNull FileContext<?> context, @NotNull FileElement e, @Nullable FileObject menuContext) {
-        return null;
-    }
-
-    public @Nullable NumberProvider parsePageIndexes(@NotNull FileContext<?> context, @NotNull FileElement e, @Nullable FileObject menuContext){
+public abstract class PagedMenuModuleBuilder implements SimpleMenuModuleBuilder {
+    public @Nullable NumberProvider parsePageIndexes(@NotNull FileContext<?> context,
+                                                     @NotNull FileElement e,
+                                                     @Nullable FileObject menuContext){
         if(!(e instanceof FileObject o)) return null;
         return o.getObject(NumberProvider.class, "indexes");
-    }
-
-    @Override
-    public @NotNull Key key() {
-        return Crux.key("paged");
     }
 }
