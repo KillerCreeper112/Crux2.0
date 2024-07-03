2da5eba35de87c60b62f4db71f954cb6b35efb0e
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-07-03 13:36:10 -0400

    ye

--- CruxMain/src/main/java/killercreepr/crux/util/CruxObjectValidate.java
+++ CruxMain/src/main/java/killercreepr/crux/util/CruxObjectValidate.java
@@ -1,13 +1,20 @@
 package killercreepr.crux.util;
 
 public class CruxObjectValidate {
-    public static void checkNotNull(Object... objects){
+    public static boolean checkNull(Object... objects){
+        for(Object o : objects){
+            if(o==null) return true;
+        }
+        return false;
+    }
+
+    public static void requireNotNull(Object... objects){
         for(Object o : objects){
             if(o==null) throw new NullPointerException();
         }
     }
 
-    public static void checkNotNull(String msg, Object... objects){
+    public static void requireNotNull(String msg, Object... objects){
         for(Object o : objects){
             if(o==null) throw new NullPointerException(msg);
         }
