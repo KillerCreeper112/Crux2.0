afc6250218bc558531ddb8ff225eca6e8af2038e
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-07-08 14:11:29 -0400

    ye

--- CruxMain/src/main/java/killercreepr/crux/command/argument/CruxKeyArgument.java
+++ CruxMain/src/main/java/killercreepr/crux/command/argument/CruxKeyArgument.java
@@ -2,23 +2,30 @@ package killercreepr.crux.command.argument;
 
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.arguments.ArgumentType;
-import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
 import io.papermc.paper.command.brigadier.argument.CustomArgumentType;
 import killercreepr.crux.Crux;
 import net.kyori.adventure.key.Key;
-import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 
-public class CruxKeyArgument implements CustomArgumentType<Key, String> {
+public class CruxKeyArgument implements CustomArgumentType<Key, Key> {
     @Override
     public @NotNull Key parse(@NotNull StringReader reader) throws CommandSyntaxException {
-        return Crux.key(getNativeType().parse(reader));
+        String string = readString(reader);
+        return Crux.key(string);
+    }
+
+    public String readString(@NotNull StringReader reader) {
+        final int start = reader.getCursor();
+        while (reader.canRead() && reader.peek() != ' ') {
+            reader.skip();
+        }
+        return reader.getString().substring(start, reader.getCursor());
     }
 
     @Override
-    public @NotNull ArgumentType<String> getNativeType() {
-        return StringArgumentType.string();
+    public @NotNull ArgumentType<Key> getNativeType() {
+        return ArgumentTypes.key();
     }
 }
