38705738a612b96a26d3e6a49f44d3e6c29b2ac9
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-07-11 11:32:34 -0400

    ye

--- CruxStructures/src/main/java/killercreepr/cruxstructures/commands/argument/StructureArgument.java
+++ CruxStructures/src/main/java/killercreepr/cruxstructures/commands/argument/StructureArgument.java
@@ -1,6 +1,9 @@
 package killercreepr.cruxstructures.commands.argument;
 
 import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
 import io.papermc.paper.command.brigadier.argument.CustomArgumentType;
 import killercreepr.cruxstructures.registries.StructureRegistries;
@@ -9,11 +12,20 @@ import net.kyori.adventure.key.Key;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
+import java.util.concurrent.CompletableFuture;
 
 public class StructureArgument implements CustomArgumentType.Converted<Structure, Key> {
     @Override
     public @NotNull Structure convert(@NotNull Key nativeType) {
         return Objects.requireNonNull(StructureRegistries.STRUCTURES.get(nativeType), "Structure '" + nativeType + "' not found!");
+    }
+
+    @Override
+    public @NotNull <S> CompletableFuture<Suggestions> listSuggestions(@NotNull CommandContext<S> context, @NotNull SuggestionsBuilder builder) {
+        for(Structure s : StructureRegistries.STRUCTURES){
+            builder.suggest(s.key().asString());
+        }
+        return builder.buildFuture();
     }
 
     @Override
