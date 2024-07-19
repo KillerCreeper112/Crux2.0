9a2e34743053a62f22e235b547fab26eb30a365d
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-07-19 17:10:48 -0400

    ye

---
+++ CruxMain/src/main/java/killercreepr/crux/data/util/BlockFaceRotation.java
@@ -0,0 +1,35 @@
+package killercreepr.crux.data.util;
+
+import org.bukkit.block.BlockFace;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BlockFaceRotation {
+    private static final Map<BlockFace, BlockFace[]> rotationMap = new HashMap<>(){{
+        put(BlockFace.NORTH, new BlockFace[]{BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST});
+        put(BlockFace.EAST, new BlockFace[]{BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST, BlockFace.NORTH});
+        put(BlockFace.SOUTH, new BlockFace[]{BlockFace.SOUTH, BlockFace.WEST, BlockFace.NORTH, BlockFace.EAST});
+        put(BlockFace.WEST, new BlockFace[]{BlockFace.WEST, BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH});
+    }};
+
+    public static BlockFace rotate(@NotNull BlockFace center, @NotNull BlockFace direction) {
+        BlockFace[] rotations = rotationMap.get(center);
+        return switch (direction) {
+            case NORTH -> rotations[0];
+            case EAST -> rotations[1];
+            case SOUTH -> rotations[2];
+            case WEST -> rotations[3];
+            default -> throw new IllegalArgumentException("Unsupported direction: " + direction);
+        };
+    }
+
+    public static BlockFace rotateLeft(@NotNull BlockFace center) {
+        return rotate(center, BlockFace.NORTH);
+    }
+
+    public static BlockFace rotateRight(@NotNull BlockFace center) {
+        return rotate(center, BlockFace.EAST);
+    }
+}
