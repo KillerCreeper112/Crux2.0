89b20568dd894274902acc104563ec6e64faa426
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-07-21 19:11:29 -0400

    ye

--- CruxMain/src/main/java/killercreepr/crux/command/argument/CruxKeyedArgument.java
+++ CruxMain/src/main/java/killercreepr/crux/command/argument/CruxKeyedArgument.java
@@ -1,16 +1,26 @@
 package killercreepr.crux.command.argument;
 
+import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
 import io.papermc.paper.command.brigadier.argument.CustomArgumentType;
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.key.Keyed;
 import org.jetbrains.annotations.NotNull;
 
 public interface CruxKeyedArgument<T extends Keyed> extends CustomArgumentType<T, Key> {
+    @Override
+    default @NotNull T parse(@NotNull StringReader reader) throws CommandSyntaxException {
+        return parse(CruxCmdArguments.CRUX_KEY.parse(reader));
+    }
+
     @NotNull T parse(@NotNull Key key);
+
 
     @Override
     default @NotNull ArgumentType<Key> getNativeType() {
-        return CruxCmdArguments.CRUX_KEY;
+        return ArgumentTypes.key();
     }
 }
