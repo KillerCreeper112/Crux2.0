c232cdb3acb061af8fceb86426c992ef9be592f6
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-08-24 13:21:48 -0400

    ye

--- CruxWorlds/src/main/java/killercreepr/cruxworlds/world/manager/CruxWorldManager.java
+++ CruxWorlds/src/main/java/killercreepr/cruxworlds/world/manager/CruxWorldManager.java
@@ -13,6 +13,21 @@ public interface CruxWorldManager {
     @Nullable
     CruxWorld getWorld(@NotNull String name);
     @Nullable CruxWorld getWorld(@NotNull UUID uuid);
+
+    default <T extends CruxWorld> @Nullable T getWorld(@NotNull String name, @NotNull Class<T> type){
+        CruxWorld world = getWorld(name);
+        if(world==null) return null;
+        if(type.isAssignableFrom(world.getClass())) return type.cast(world);
+        throw new IllegalStateException("CruxWorld cannot be cast to " + type.getSimpleName() + "! (" + world + ")");
+    }
+
+    default <T extends CruxWorld> @Nullable T getWorld(@NotNull UUID uuid, @NotNull Class<T> type){
+        CruxWorld world = getWorld(uuid);
+        if(world==null) return null;
+        if(type.isAssignableFrom(world.getClass())) return type.cast(world);
+        throw new IllegalStateException("CruxWorld cannot be cast to " + type.getSimpleName() + "! (" + world + ")");
+    }
+
     @NotNull
     Collection<CruxWorld> getWorlds();
 
