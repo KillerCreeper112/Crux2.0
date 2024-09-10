d16f3aa5a2eeefcbb9abb028f58344cd7c61dd19
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-09-10 15:05:41 -0400

    ye

--- CruxStructures/src/main/java/killercreepr/cruxstructures/config/module/FileWallsModule.java
+++ CruxStructures/src/main/java/killercreepr/cruxstructures/config/module/FileWallsModule.java
@@ -1,29 +1,43 @@
 package killercreepr.cruxstructures.config.module;
 
 import com.google.common.reflect.TypeToken;
+import killercreepr.crux.loot.LootTable;
 import killercreepr.crux.valueproviders.number.NumberProvider;
+import killercreepr.cruxconfig.config.bukkit.handler.impl.loot.FileSimpleLootTable;
 import killercreepr.cruxconfig.config.common.FileContext;
 import killercreepr.cruxconfig.config.common.FileRegistry;
 import killercreepr.cruxconfig.config.common.element.FileElement;
 import killercreepr.cruxconfig.config.common.element.FileObject;
 import killercreepr.cruxconfig.config.common.handler.PureYamlFileHandler;
-import killercreepr.cruxstructures.structure.generation.StructureGenerator;
 import killercreepr.cruxstructures.structure.module.standard.WallsModule;
+import net.kyori.adventure.key.Key;
 import org.bukkit.block.BlockFace;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class FileWallsModule extends PureYamlFileHandler<WallsModule> {
+    protected final @NotNull FileSimpleLootTable<Key> fileSimpleLootTable = new FileSimpleLootTable<>(Key.class);
     @Override
     public @Nullable WallsModule deserializeFromFile(@NotNull FileContext<?> ctx, @NotNull FileElement e) {
         if(!(e instanceof FileObject o)) return null;
         FileRegistry registry = ctx.getRegistry();
-        Map<BlockFace, StructureGenerator> walls = registry.deserializeFromFile(
-            new TypeToken<Map<BlockFace, StructureGenerator>>(){}.getType(), o.get("walls")
-        );
-        if(walls == null) return null;
+        Map<BlockFace, LootTable<Key>> walls = new HashMap<>();
+        if(!(o.get("walls") instanceof FileObject oo)) return null;
+
+        oo.forEach((key, value) ->{
+            LootTable<Key> lootTable = fileSimpleLootTable.deserializeFromFile(ctx, value);
+            if(lootTable == null) return;
+            BlockFace face;
+            try{
+                face = BlockFace.valueOf(key.toUpperCase());
+            }catch (IllegalArgumentException ignored){ return; }
+            walls.put(face, lootTable);
+        });
+        if(walls.isEmpty()) return null;
+
         Map<BlockFace, NumberProvider> wallSpacing = registry.deserializeFromFile(
             new TypeToken<Map<BlockFace, NumberProvider>>(){}.getType(), o.get("wall_spacing")
         );
@@ -39,7 +53,8 @@ public class FileWallsModule extends PureYamlFileHandler<WallsModule> {
             walls,
             registry.deserializeFromFile(NumberProvider.class, o.get("default_wall_spacing")),
             wallSpacing,
-            rotationType
+            rotationType,
+            registry.deserializeFromFile(NumberProvider.class, o.get("wall_max_y_check"))
         );
     }
 }
--- CruxStructures/src/main/java/killercreepr/cruxstructures/structure/module/standard/WallsModule.java
+++ CruxStructures/src/main/java/killercreepr/cruxstructures/structure/module/standard/WallsModule.java
@@ -1,27 +1,38 @@
 package killercreepr.cruxstructures.structure.module.standard;
 
+import killercreepr.crux.Crux;
+import killercreepr.crux.data.DataExchange;
 import killercreepr.crux.data.world.CruxPosition;
+import killercreepr.crux.loot.LootContext;
+import killercreepr.crux.loot.LootTable;
 import killercreepr.crux.util.CruxMath;
 import killercreepr.crux.valueproviders.number.NumberProvider;
+import killercreepr.cruxstructures.registries.StructureRegistries;
 import killercreepr.cruxstructures.structure.Structure;
-import killercreepr.cruxstructures.structure.generation.StructureGenerator;
 import killercreepr.cruxstructures.structure.module.StructureModule;
+import net.kyori.adventure.key.Key;
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
 
 public class WallsModule implements StructureModule {
-    protected final @NotNull Map<BlockFace, StructureGenerator> walls;
+    protected final @NotNull Map<BlockFace, LootTable<Key>> walls;
     protected final @NotNull NumberProvider defaultWallSpacing;
     protected final @NotNull Map<BlockFace, NumberProvider> wallSpacing;
     protected final @NotNull WallRotationType wallRotationType;
-    public WallsModule(@NotNull Map<BlockFace, StructureGenerator> walls, @NotNull NumberProvider defaultWallSpacing, @NotNull Map<BlockFace, NumberProvider> wallSpacing, @NotNull WallRotationType wallRotationType) {
+    protected final @NotNull NumberProvider wallMaxYCheck;
+
+    public WallsModule(@NotNull Map<BlockFace, LootTable<Key>> walls, @NotNull NumberProvider defaultWallSpacing, @NotNull Map<BlockFace, NumberProvider> wallSpacing, @NotNull WallRotationType wallRotationType, @NotNull NumberProvider wallMaxYCheck) {
         this.walls = walls;
         this.defaultWallSpacing = defaultWallSpacing;
         this.wallSpacing = wallSpacing;
         this.wallRotationType = wallRotationType;
+        this.wallMaxYCheck = wallMaxYCheck;
     }
 
     @Override
@@ -31,7 +42,23 @@ public class WallsModule implements StructureModule {
             case NONE -> 0D;
             default -> rotation;
         };
-        walls.forEach((face, wallStructure) ->{
+        LootContext ctx = LootContext.builder()
+            .location(at)
+            .info(DataExchange.builder()
+                .put("structure", structure)
+                .put("rotation", rotation)
+                .put("structure_module", this)
+                .build())
+            .build();
+        walls.forEach((face, wallLoot) ->{
+            List<Key> keys = wallLoot.populateLoot(ctx);
+            if(keys.isEmpty()) return;
+            Structure wallStructure = StructureRegistries.STRUCTURES.get(keys.getFirst());
+            if(wallStructure== null){
+                Crux.log(Level.WARNING, "No crux Structure found for wall " + face + " (" + keys.getFirst() + ")");
+                return;
+            }
+
             int width = wallSpacing.getOrDefault(face, defaultWallSpacing).value().intValue();
 
             CruxPosition spawn = CruxPosition.location(
@@ -44,8 +71,28 @@ public class WallsModule implements StructureModule {
 
             spawn = spawn.rotateAroundY(CruxPosition.location(at), wallRotation);
 
-            structure.place(spawn.toLocation(at.getWorld()), wallRotation);
+            Block validSpawn = findValidBlock(spawn.getBlock(at.getWorld()));
+            if(validSpawn == null) return;
+
+            wallStructure.place(validSpawn.getLocation(), wallRotation);
         });
+    }
+
+    public boolean isValidBlock(Block b){
+        if(!b.isSolid()) return false;
+        Block check = b.getRelative(BlockFace.UP);
+        return check.isEmpty() || check.isPassable() || check.isReplaceable();
+    }
+
+    public Block findValidBlock(Block start){
+        if(isValidBlock(start)) return start;
+        int max = wallMaxYCheck.value().intValue();
+        for(int y = -max; y <= max; y++){
+            if(y==0) continue;
+            Block check = start.getRelative(0, y, 0);
+            if(isValidBlock(check)) return check;
+        }
+        return null;
     }
 
     public enum WallRotationType{
