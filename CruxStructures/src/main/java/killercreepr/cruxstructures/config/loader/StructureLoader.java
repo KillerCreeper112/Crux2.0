bb8c9004db6b5e338a310f5ee1a675f8d713b111
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-09-10 15:28:22 -0400

    ye

--- CruxStructures/src/main/java/killercreepr/cruxstructures/config/loader/StructureLoader.java
+++ CruxStructures/src/main/java/killercreepr/cruxstructures/config/loader/StructureLoader.java
@@ -3,11 +3,14 @@ package killercreepr.cruxstructures.config.loader;
 import killercreepr.crux.Crux;
 import killercreepr.cruxconfig.config.bukkit.loader.CfgLoader;
 import killercreepr.cruxconfig.config.common.FileContext;
+import killercreepr.cruxconfig.config.common.FileRegistry;
 import killercreepr.cruxconfig.config.common.element.FileObject;
 import killercreepr.cruxconfig.config.common.file.DataFile;
+import killercreepr.cruxconfig.config.registry.CfgRegistries;
 import killercreepr.cruxstructures.config.FileCfgFAWEStructure;
 import killercreepr.cruxstructures.registries.StructureRegistries;
 import killercreepr.cruxstructures.structure.Structure;
+import killercreepr.cruxstructures.structure.impl.CfgFAWEStructure;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -22,15 +25,20 @@ public class StructureLoader extends CfgLoader {
     @Override
     public void loadConfiguration(@NotNull DataFile cfg, @Nullable String path){
         Structure table;
-        if(path == null) table = cfg.deserialize("", Structure.class);
+        if(path == null) table = cfg.deserialize("", CfgFAWEStructure.class);
         else{
             if(!(cfg.getRoot() instanceof FileObject root)) return;
 
+            FileContext<?> fileCtx = new FileContext<>(cfg.fileRegistry());
             table = fileCfgFAWEStructure.deserializeFromFile(
-                new FileContext<>(cfg.fileRegistry()), root, Crux.key(path)
+                fileCtx, root, Crux.key(path)
             );
+            if(table==null) return;
+
+            table = cfg.fileRegistry().getParsedObjectRegistry().parse(root, fileCtx, table);
         }
         if(table == null) return;
+
         Crux.log(Level.INFO, "Registered structure: " + table.key());
         StructureRegistries.STRUCTURES.register(table);
     }
