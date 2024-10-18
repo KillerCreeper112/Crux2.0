f5fc1d17384f8366d1af349ffcf2787b0c708821
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-10-18 17:45:40 -0400

    ye

--- CruxBlocks/src/main/java/killercreepr/cruxblocks/structure/modules/UpdateCustomBlocksModule.java
+++ CruxBlocks/src/main/java/killercreepr/cruxblocks/structure/modules/UpdateCustomBlocksModule.java
@@ -1,10 +1,12 @@
 package killercreepr.cruxblocks.structure.modules;
 
 import killercreepr.crux.Crux;
+import killercreepr.crux.data.world.CruxPosition;
 import killercreepr.crux.registries.CruxRegistries;
 import killercreepr.cruxblocks.CruxBlocksModule;
 import killercreepr.cruxstructures.structure.Structure;
 import killercreepr.cruxstructures.structure.module.StructureModule;
+import killercreepr.cruxstructures.util.CruxStructureUtil;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.block.Block;
@@ -17,6 +19,7 @@ public class UpdateCustomBlocksModule implements StructureModule {
         Crux.getServer().getScheduler().runTaskAsynchronously(Crux.getMainPlugin(), task ->{
             CruxBlocksModule cruxBlocks = CruxRegistries.MODULES.getModuleOrThrow(CruxBlocksModule.class);
             structure.getBlocks(rotation).forEach(pos ->{
+                pos = CruxStructureUtil.fromStructureToWorldPos(structure, CruxPosition.location(at), pos);
                 Block block = pos.getBlock(world);
                 cruxBlocks.getActiveBlock(block);
             });
