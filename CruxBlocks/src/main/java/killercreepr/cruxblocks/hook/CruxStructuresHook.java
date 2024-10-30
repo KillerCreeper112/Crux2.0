c7645e75e104542b6f759511c90ea03536e0ebc2
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-10-30 14:35:26 -0400

    ye

--- CruxBlocks/src/main/java/killercreepr/cruxblocks/hook/CruxStructuresHook.java
+++ CruxBlocks/src/main/java/killercreepr/cruxblocks/hook/CruxStructuresHook.java
@@ -1,15 +1,24 @@
 package killercreepr.cruxblocks.hook;
 
+import com.google.common.reflect.TypeToken;
+import killercreepr.crux.data.BlockPos;
 import killercreepr.crux.registries.CruxRegistries;
+import killercreepr.cruxblocks.structure.modules.PlaceCustomBlocksModule;
 import killercreepr.cruxblocks.structure.modules.UpdateCustomBlocksModule;
 import killercreepr.cruxconfig.config.common.FileContext;
+import killercreepr.cruxconfig.config.common.FileRegistry;
 import killercreepr.cruxconfig.config.common.element.FileElement;
+import killercreepr.cruxconfig.config.common.element.FileObject;
 import killercreepr.cruxconfig.config.common.handler.PureYamlFileHandler;
 import killercreepr.cruxstructures.CruxStructuresModule;
 import killercreepr.cruxstructures.config.FileStructureModule;
 import killercreepr.cruxstructures.structure.module.StructureModule;
+import net.kyori.adventure.key.Key;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.Map;
 
 public class CruxStructuresHook {
     public static void register(){
@@ -19,6 +28,19 @@ public class CruxStructuresHook {
             @Override
             public @Nullable StructureModule deserializeFromFile(@NotNull FileContext<?> context, @NotNull FileElement e) {
                 return new UpdateCustomBlocksModule();
+            }
+        });
+
+        fileStructureModule.typeHandlers().register("place_custom_blocks", new PureYamlFileHandler<StructureModule>() {
+            @Override
+            public @Nullable StructureModule deserializeFromFile(@NotNull FileContext<?> context, @NotNull FileElement e) {
+                if(!(e instanceof FileObject o)) return null;
+                FileRegistry registry = context.getRegistry();
+                Map<Key, Collection<BlockPos>> blocks = registry.deserializeFromFile(
+                    new TypeToken<Map<Key, Collection<BlockPos>>>(){}.getType(),
+                    o.get("blocks")
+                );
+                return new PlaceCustomBlocksModule(blocks);
             }
         });
     }
