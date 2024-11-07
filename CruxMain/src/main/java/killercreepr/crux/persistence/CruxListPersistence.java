8ade7af9f5c4d0be85742a8a4dd4ab6a7a6e444c
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-11-07 16:50:40 -0500

    ye

--- CruxMain/src/main/java/killercreepr/crux/persistence/CruxListPersistence.java
+++ CruxMain/src/main/java/killercreepr/crux/persistence/CruxListPersistence.java
@@ -5,6 +5,7 @@ import killercreepr.crux.loot.item.ItemLootTable;
 import killercreepr.crux.persistence.impl.ListTagType;
 import net.kyori.adventure.key.Key;
 import org.bukkit.Location;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.potion.PotionEffect;
 
@@ -14,6 +15,7 @@ public class CruxListPersistence {
     public final ListTagType<PersistentDataContainer, Location> LOCATION = new ListTagType<>(CruxPersistence.LOCATION);
     public final ListTagType<PersistentDataContainer, ToolComponent> TOOL_COMPONENT = new ListTagType<>(CruxPersistence.TOOL_COMPONENT);
     public final ListTagType<String, Key> KEY = new ListTagType<>(CruxPersistence.CRUX_KEY);
+    public final ListTagType<String, ItemStack> ITEM_STACK = new ListTagType<>(CruxPersistence.ITEM_STACK);
     public final ListTagType<PersistentDataContainer, ToolComponent.Rule> TOOL_COMPONENT_RULE = new ListTagType<>(CruxPersistence.TOOL_COMPONENT_RULE);
     public final ListTagType<String, ItemLootTable> ITEM_LOOT_TABLE = new ListTagType<>(CruxPersistence.ITEM_LOOT_TABLE);
 }
--- CruxMain/src/main/java/killercreepr/crux/persistence/CruxPersistence.java
+++ CruxMain/src/main/java/killercreepr/crux/persistence/CruxPersistence.java
@@ -12,6 +12,7 @@ public class CruxPersistence {
     public static final UUIDTagType UUID = new UUIDTagType();
     public static final PotionEffectTagType POTION_EFFECT = new PotionEffectTagType();
     public static final CruxKeyTagType CRUX_KEY = new CruxKeyTagType();
+    public static final ItemStackTagType ITEM_STACK = new ItemStackTagType();
     public static final LocationTagType LOCATION = new LocationTagType();
     public static final VectorTagType VECTOR = new VectorTagType();
     public static final BlockPosTagType BLOCK_POS = new BlockPosTagType();
