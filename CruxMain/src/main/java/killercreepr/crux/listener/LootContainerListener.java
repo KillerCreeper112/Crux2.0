26fd5ea3449ab21bcc3cd361acd217915a1e73ea
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-11-20 09:26:07 -0500

    ye

--- CruxMain/src/main/java/killercreepr/crux/listener/LootContainerListener.java
+++ CruxMain/src/main/java/killercreepr/crux/listener/LootContainerListener.java
@@ -3,12 +3,16 @@ package killercreepr.crux.listener;
 import killercreepr.crux.loot.LootContext;
 import killercreepr.crux.loot.item.ItemLootTable;
 import killercreepr.crux.persistence.CruxPersist;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDropItemEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.inventory.Inventory;
@@ -48,4 +52,26 @@ public class LootContainerListener implements Listener {
         LootContext ctx = LootContext.builder().looted(b).location(inv.getLocation()).looter(p).build();
         lootTable.forEach(loot -> loot.fillInventory(inv, ctx));
     }
+
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
+    public void onBlockDropItem(BlockDropItemEvent event) {
+        BlockState state = event.getBlockState();
+        if(!(state instanceof PersistentDataHolder data)) return;
+        HumanEntity p = event.getPlayer();
+        List<ItemLootTable> lootTable = CruxPersist.ITEM_LOOT_TABLES.get(data, null);
+        if(lootTable == null) return;
+        Long time = CruxPersist.LOOT_GENERATED_TIME.get(data, null);
+        if(time != null) return;
+        Block b = event.getBlock();
+        Location spawn = b.getLocation().toCenterLocation();
+
+        World world = b.getWorld();
+
+        LootContext ctx = LootContext.builder().looted(b).location(b.getLocation()).looter(p).build();
+        List<Item> itemDrops = event.getItems();
+        lootTable.forEach(loot -> loot.populateLoot(ctx).forEach(item ->{
+            itemDrops.add(world.dropItem(spawn, item));
+        }));
+    }
+
 }
