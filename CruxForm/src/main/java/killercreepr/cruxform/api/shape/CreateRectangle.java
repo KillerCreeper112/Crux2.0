3acd8ee4c8a03041a8fdbe8381420476c29135ee
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-12-12 10:00:02 -0500

    ye

--- CruxForm/src/main/java/killercreepr/cruxform/api/shape/CreateRectangle.java
+++ CruxForm/src/main/java/killercreepr/cruxform/api/shape/CreateRectangle.java
@@ -3,7 +3,10 @@ package killercreepr.cruxform.api.shape;
 import killercreepr.crux.api.data.Holder;
 import killercreepr.crux.api.math.CruxLocation;
 import killercreepr.crux.api.valueproviders.number.NumberProvider;
+import killercreepr.crux.core.util.CruxedBoundingBox;
 import killercreepr.cruxform.core.shape.SimpleCreateRectangle;
+import org.bukkit.util.BoundingBox;
+import org.jetbrains.annotations.NotNull;
 
 public interface CreateRectangle extends CreateShape{
     static Builder builder(){
@@ -24,6 +27,18 @@ public interface CreateRectangle extends CreateShape{
             return spacing(NumberProvider.constant(spacing));
         }
         Builder inverted(boolean inverted);
+        default Builder boundingBox(@NotNull BoundingBox box){
+            return pos1(CruxLocation.location(box.getMinX(), box.getMinY(), box.getMinZ()))
+                .pos2(CruxLocation.location(box.getMaxX(), box.getMaxY(), box.getMaxZ()));
+        }
+        default Builder boundingBox(CruxedBoundingBox box){
+            return boundingBox(box.box());
+        }
+        default Builder centerRange(@NotNull CruxLocation center, double size){
+            CruxLocation pos1 = center.add(-size, -size, -size);
+            CruxLocation pos2 = center.add(size, size, size);
+            return pos1(pos1).pos2(pos2);
+        }
         CreateRectangle build();
     }
 
