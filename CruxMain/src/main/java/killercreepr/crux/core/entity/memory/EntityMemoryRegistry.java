b05f166f061d8ffe4e95f72d4d7094e62baf9d35
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2024-12-13 20:25:17 -0500

    ye

--- CruxMain/src/main/java/killercreepr/crux/core/entity/memory/EntityMemoryRegistry.java
+++ CruxMain/src/main/java/killercreepr/crux/core/entity/memory/EntityMemoryRegistry.java
@@ -4,9 +4,22 @@ import killercreepr.crux.api.entity.memory.EntityMemory;
 import killercreepr.crux.core.registry.SimpleMappedRegistry;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class EntityMemoryRegistry<V extends EntityMemory> extends SimpleMappedRegistry<UUID, V> {
+    public EntityMemoryRegistry(@NotNull Map<UUID, V> map) {
+        super(map);
+    }
+
+    /**
+     * Creates a registry using a HashMap.
+     */
+    public EntityMemoryRegistry() {
+        this(new ConcurrentHashMap<>());
+    }
+
     @Override
     public <E extends V> @NotNull E register(@NotNull E object) {
         return register(object.getUUID(), object);
