4aab4a326dcd6e88dbe6db97972b62f3486b366d
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2025-01-03 16:51:38 -0500

    ye

--- CruxMain/src/main/java/killercreepr/crux/api/data/User.java
+++ CruxMain/src/main/java/killercreepr/crux/api/data/User.java
@@ -1,11 +1,15 @@
 package killercreepr.crux.api.data;
 
 import killercreepr.crux.core.data.SimpleUser;
+import org.bukkit.entity.Entity;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
 
 public interface User {
+    static User user(@NotNull Entity entity){
+        return user(entity.getUniqueId(), entity.getName());
+    }
     static User user(@NotNull UUID uuid, @NotNull String name){
         return new SimpleUser(uuid, name);
     }
