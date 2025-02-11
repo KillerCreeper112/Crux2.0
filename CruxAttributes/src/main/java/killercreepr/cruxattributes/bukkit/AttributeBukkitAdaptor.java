4d633413234906fffe6d91cc145bef7e1557d0b5
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2025-02-10 21:58:49 -0500

    ye

--- CruxAttributes/src/main/java/killercreepr/cruxattributes/bukkit/AttributeBukkitAdaptor.java
+++ CruxAttributes/src/main/java/killercreepr/cruxattributes/bukkit/AttributeBukkitAdaptor.java
@@ -1,7 +1,9 @@
 package killercreepr.cruxattributes.bukkit;
 
 import killercreepr.cruxattributes.api.equipment.CruxSlot;
+import killercreepr.cruxattributes.api.equipment.CruxSlotGroup;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.EquipmentSlotGroup;
 import org.jetbrains.annotations.Nullable;
 
 public class AttributeBukkitAdaptor {
@@ -15,6 +17,32 @@ public class AttributeBukkitAdaptor {
         EquipmentSlot.HAND,
         EquipmentSlot.OFF_HAND
     };
+
+    public static @Nullable CruxSlotGroup adapt(EquipmentSlotGroup slot) {
+        if (slot == EquipmentSlotGroup.ANY) return CruxSlotGroup.ANY;
+        if (slot == EquipmentSlotGroup.HEAD) return CruxSlotGroup.HEAD;
+        if (slot == EquipmentSlotGroup.CHEST) return CruxSlotGroup.CHEST;
+        if (slot == EquipmentSlotGroup.LEGS) return CruxSlotGroup.LEGS;
+        if (slot == EquipmentSlotGroup.FEET) return CruxSlotGroup.FEET;
+        if (slot == EquipmentSlotGroup.HAND) return CruxSlotGroup.HAND;
+        if (slot == EquipmentSlotGroup.MAINHAND) return CruxSlotGroup.MAIN_HAND;
+        if (slot == EquipmentSlotGroup.OFFHAND) return CruxSlotGroup.OFF_HAND;
+        if (slot == EquipmentSlotGroup.ARMOR) return CruxSlotGroup.ARMOR;
+        return null;
+    }
+
+    public static @Nullable EquipmentSlotGroup adapt(CruxSlotGroup slot) {
+        if(slot == CruxSlotGroup.ANY) return EquipmentSlotGroup.ANY;
+        if(slot == CruxSlotGroup.HEAD) return EquipmentSlotGroup.HEAD;
+        if(slot == CruxSlotGroup.CHEST) return EquipmentSlotGroup.CHEST;
+        if(slot == CruxSlotGroup.LEGS) return EquipmentSlotGroup.LEGS;
+        if(slot == CruxSlotGroup.FEET) return EquipmentSlotGroup.FEET;
+        if(slot == CruxSlotGroup.HAND) return EquipmentSlotGroup.HAND;
+        if(slot == CruxSlotGroup.MAIN_HAND) return EquipmentSlotGroup.MAINHAND;
+        if(slot == CruxSlotGroup.OFF_HAND) return EquipmentSlotGroup.OFFHAND;
+        if(slot == CruxSlotGroup.ARMOR) return EquipmentSlotGroup.ARMOR;
+        return null;
+    }
 
     public static @Nullable EquipmentSlot adapt(CruxSlot slot) {
         try {
