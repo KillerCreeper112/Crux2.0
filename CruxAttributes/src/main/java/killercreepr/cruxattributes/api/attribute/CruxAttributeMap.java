17c720462d5eb2a8673399b42dd4a121dec59b2c
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2025-02-10 21:19:41 -0500

    ye

--- CruxAttributes/src/main/java/killercreepr/cruxattributes/api/attribute/CruxAttributeMap.java
+++ CruxAttributes/src/main/java/killercreepr/cruxattributes/api/attribute/CruxAttributeMap.java
@@ -1,5 +1,6 @@
 package killercreepr.cruxattributes.api.attribute;
 
+import killercreepr.cruxattributes.core.attribute.map.SimpleCruxAttributeMap;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
@@ -7,6 +8,19 @@ import java.util.Map;
 import java.util.function.BiConsumer;
 
 public interface CruxAttributeMap {
+    static CruxAttributeMap emptyMap(){
+        return EMPTY;
+    }
+    static CruxAttributeMap attributeMap(Map<CruxAttribute, Double> map){
+        return new SimpleCruxAttributeMap(map);
+    }
+
+    static CruxAttributeMap attributeMap(CruxAttribute attribute, double value){
+        return attributeMap(Map.of(attribute, value));
+    }
+
+    CruxAttributeMap EMPTY = new SimpleCruxAttributeMap(Map.of());
+
     double getValue(@NotNull CruxAttribute attribute);
     boolean hasAttribute(@NotNull CruxAttribute attribute);
     @Contract(pure = true)
