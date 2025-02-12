961cd1dd6c8c76a9b26d12d9c4944bf4d6145d70
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2025-02-11 22:43:38 -0500

    ye

---
+++ CruxMain/src/main/java/killercreepr/crux/api/data/ItemIcon.java
@@ -0,0 +1,8 @@
+package killercreepr.crux.api.data;
+
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+public interface ItemIcon {
+    @NotNull ItemStack getItem();
+}
