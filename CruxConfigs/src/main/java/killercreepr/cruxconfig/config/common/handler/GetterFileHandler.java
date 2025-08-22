dfc2b0c4c0e97b5f1a61416c61e06e878557be7e
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2025-08-22 09:30:23 -0400

    ye

---
+++ CruxConfigs/src/main/java/killercreepr/cruxconfig/config/common/handler/GetterFileHandler.java
@@ -0,0 +1,17 @@
+package killercreepr.cruxconfig.config.common.handler;
+
+import killercreepr.cruxconfig.config.common.FileContext;
+import killercreepr.cruxconfig.config.common.element.FileElement;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class GetterFileHandler<T> extends SimpleFileHandler<T> {
+    @Override
+    public @NotNull FileElement serializeToFile(@NotNull FileContext<?> context, @NotNull T object) {
+        throw new RuntimeException(object.getClass() + " does not have a serialize method set!");
+    }
+
+    @Override
+    public @NotNull String jsonSerializerID() {
+        throw new RuntimeException(getClass().getSimpleName() + " does not have a json serializer ID set.");
+    }
+}
