d52e0203b74c362efd8801ad8549bf007be1e152
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2025-08-27 15:04:34 -0400

    ye

--- CruxMain/src/main/java/killercreepr/crux/api/data/User.java
+++ CruxMain/src/main/java/killercreepr/crux/api/data/User.java
@@ -1,5 +1,6 @@
 package killercreepr.crux.api.data;
 
+import killercreepr.crux.core.Crux;
 import killercreepr.crux.core.data.SimpleUser;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Entity;
@@ -19,6 +20,14 @@ public interface User {
     }
     static User userName(String name){
         return new SimpleUser(null, name);
+    }
+    static User attemptUserFromString(String string, boolean useOfflinePlayer){
+        try{
+            UUID uuid = UUID.fromString(string);
+            String name = useOfflinePlayer ? Crux.getServer().getOfflinePlayer(uuid).getName() : null;
+            return User.user(uuid, name);
+        }catch (IllegalArgumentException ignored){}
+        return userName(string);
     }
 
     UUID uuid();
