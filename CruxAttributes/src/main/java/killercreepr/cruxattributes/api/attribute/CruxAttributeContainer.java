071f10fee541c9ce6a21df0940aea053d20592c4
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2025-08-29 14:16:10 -0400

    ye

--- CruxAttributes/src/main/java/killercreepr/cruxattributes/api/attribute/CruxAttributeContainer.java
+++ CruxAttributes/src/main/java/killercreepr/cruxattributes/api/attribute/CruxAttributeContainer.java
@@ -5,6 +5,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
 public interface CruxAttributeContainer extends CruxAttributeAccessor {
@@ -18,6 +19,14 @@ public interface CruxAttributeContainer extends CruxAttributeAccessor {
 
     static CruxAttributeContainer container(@NotNull Map<CruxAttribute, CruxAttributeInstance> map){
         return new SimpleCruxAttributeContainer(map);
+    }
+
+    static CruxAttributeContainer baseContainer(@NotNull Map<CruxAttribute, Double> map){
+        Map<CruxAttribute, CruxAttributeInstance> instances = new HashMap<>();
+        map.forEach((att, base) ->{
+            instances.put(att, CruxAttributeInstance.instance(att, CruxAttributeModifier.baseModifier(base)));
+        });
+        return container(instances);
     }
 
     static CruxAttributeContainer empty(){
