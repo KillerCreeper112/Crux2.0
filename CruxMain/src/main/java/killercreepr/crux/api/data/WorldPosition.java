a69252d966f4df50ef5c7181b3c624fb6f9b37a2
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2025-11-07 16:04:54 -0500

    ye

--- CruxMain/src/main/java/killercreepr/crux/api/data/WorldPosition.java
+++ CruxMain/src/main/java/killercreepr/crux/api/data/WorldPosition.java
@@ -1,9 +1,17 @@
 package killercreepr.crux.api.data;
 
 import killercreepr.crux.api.math.CruxPosition;
+import killercreepr.crux.core.Crux;
 import net.kyori.adventure.key.Key;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.jetbrains.annotations.NotNull;
 
 public interface WorldPosition extends CruxPosition {
     @NotNull Key worldKey();
+
+    default Location toLocation(){
+        World world = Crux.getServer().getWorld(worldKey());
+        return toLocation(world);
+    }
 }
