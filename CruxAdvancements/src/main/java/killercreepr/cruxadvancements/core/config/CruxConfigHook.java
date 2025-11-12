5074900990198f932342a3167a96c29e38d22d80
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2025-11-11 21:21:38 -0500

    ye

--- CruxAdvancements/src/main/java/killercreepr/cruxadvancements/core/config/CruxConfigHook.java
+++ CruxAdvancements/src/main/java/killercreepr/cruxadvancements/core/config/CruxConfigHook.java
@@ -320,6 +320,15 @@ public class CruxConfigHook {
                 return new BrewObjective(data, maxProgress);
             }
         });
+
+        fileAdvancementObjective.registerCustomHandler(new FileSimpleAdvanceObjective<>(Crux.key("dummy")) {
+            @Override
+            public @Nullable AdvancementObjective deserializeFromFile(@NotNull FileContext<?> ctx, @NotNull FileObject e, @NotNull ObjectiveCommonData data) {
+                Integer maxProgress = e.getObject(Integer.class, "amount");
+                if(maxProgress==null) maxProgress = 1;
+                return new DummyObjective(data, maxProgress);
+            }
+        });
     }
 
     public static void registerHandlers(){
