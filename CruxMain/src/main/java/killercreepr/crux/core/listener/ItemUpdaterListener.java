c117a7763f49864727283be6144043848e15d806
KillerCreeper <43390898+KillerCreeper112@users.noreply.github.com>
2025-12-26 17:43:44 -0500

    ye

--- CruxMain/src/main/java/killercreepr/crux/core/listener/ItemUpdaterListener.java
+++ CruxMain/src/main/java/killercreepr/crux/core/listener/ItemUpdaterListener.java
@@ -2,11 +2,18 @@ package killercreepr.crux.core.listener;
 
 import killercreepr.crux.api.event.CruxItemLootGenerateEvent;
 import killercreepr.crux.core.Crux;
+import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDispenseLootEvent;
+import org.bukkit.event.block.BlockDropItemEvent;
+import org.bukkit.event.block.CrafterCraftEvent;
+import org.bukkit.event.enchantment.EnchantItemEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.EntityDropItemEvent;
+import org.bukkit.event.inventory.PrepareItemCraftEvent;
+import org.bukkit.event.inventory.SmithItemEvent;
 import org.bukkit.event.world.LootGenerateEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -39,4 +46,34 @@ public class ItemUpdaterListener implements Listener {
         }
     }
 
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onCrafterCraft(CrafterCraftEvent event) {
+        event.setResult(Crux.handlers().item().update(event.getResult()));
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onPrepareItemCraft(PrepareItemCraftEvent event) {
+        var result = event.getInventory().getResult();
+        if(result == null) return;
+        event.getInventory().setResult(Crux.handlers().item().update(result));
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onEntityDropItem(EntityDropItemEvent event) {
+        event.getItemDrop().setItemStack(
+            Crux.handlers().item().update(event.getItemDrop().getItemStack())
+        );
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onBlockDropItem(BlockDropItemEvent event) {
+        for (Item item : event.getItems()) {
+            item.setItemStack(Crux.handlers().item().update(item.getItemStack()));
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onEnchantItem(EnchantItemEvent event) {
+        Crux.handlers().item().update(event.getItem());
+    }
 }
--- CruxWorlds/src/main/java/killercreepr/cruxworlds/core/component/EntitySpawnEquipment.java
+++ CruxWorlds/src/main/java/killercreepr/cruxworlds/core/component/EntitySpawnEquipment.java
@@ -1,18 +1,24 @@
 package killercreepr.cruxworlds.core.component;
 
+import killercreepr.crux.api.item.CruxItem;
 import killercreepr.crux.api.loot.LootContext;
 import killercreepr.crux.api.loot.LootTable;
 import killercreepr.crux.api.text.context.TextParserContext;
+import killercreepr.crux.core.util.CruxAttributeUtil;
 import killercreepr.crux.core.util.CruxCollection;
+import killercreepr.crux.core.util.CruxInv;
+import killercreepr.crux.core.util.CruxTeamUtil;
 import killercreepr.cruxworlds.api.component.EntitySpawnComponent;
 import killercreepr.cruxworlds.api.world.entity.SpawnContext;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.EquipmentSlotGroup;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.Map;
 
 public class EntitySpawnEquipment implements EntitySpawnComponent {
@@ -35,9 +41,43 @@ public class EntitySpawnEquipment implements EntitySpawnComponent {
             .build();
 
         equipment.forEach((keyObject, table) ->{
+            String id = textCtx.deserializeString(keyObject.toString()).toUpperCase();
+
+            switch (id){
+                case "ARMOR" ->{
+                    List<ItemStack> list = table.populateLoot(lootCtx);
+                    if(list.isEmpty()) return;
+                    int index = -1;
+                    for(EquipmentSlot slot : List.of(EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET)){
+                        index++;
+                        if(index >= list.size()) break;
+                        if(!living.canUseEquipmentSlot(slot)) continue;
+                        ItemStack item = list.get(index);
+                        //todo may need to change this or something.
+                        if(CruxItem.isEmpty(item)) continue;
+                        equip.setItem(slot, item);
+                    }
+                    return;
+                }
+                case "HANDS" ->{
+                    List<ItemStack> list = table.populateLoot(lootCtx);
+                    if(list.isEmpty()) return;
+                    int index = -1;
+                    for(EquipmentSlot slot : List.of(EquipmentSlot.HAND, EquipmentSlot.OFF_HAND)){
+                        index++;
+                        if(index >= list.size()) break;
+                        if(!living.canUseEquipmentSlot(slot)) continue;
+                        ItemStack item = list.get(index);
+                        if(CruxItem.isEmpty(item)) continue;
+                        equip.setItem(slot, item);
+                    }
+                    return;
+                }
+            }
+
             EquipmentSlot slot;
             try{
-                slot = EquipmentSlot.valueOf(textCtx.deserializeString(keyObject.toString()).toUpperCase());
+                slot = EquipmentSlot.valueOf(id);
             } catch (IllegalArgumentException ex) {
                 return;
             }
